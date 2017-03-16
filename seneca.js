e18e1ddb2901af55ce87eda94fe00930757b4867
David Humphrey (:humph) david.humphrey@senecacollege.ca <david.humphrey@senecacollege.ca>
2017-03-16 12:11:48 -0400

    Add rest of lab files

---
+++ seneca.js
@@ -0,0 +1,15 @@
+/**
+ * Given a string `email`, return `true` if the string is in the form
+ * of a valid Seneca College email address, `false` othewise.
+ */
+exports.isValidEmail = function(email) {
+    // TODO: needs to be implemented
+};
+
+/**
+ * Given a string `name`, return a formatted Seneca email address for
+ * this person. NOTE: the email doesn't need to be real/valid/active.
+ */
+exports.formatSenecaEmail = function(name) {
+    // TODO: needs to be implemented
+};
